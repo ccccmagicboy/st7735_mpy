3c6efd35adacd0b33cf7f69c1b5778291235d3b0
cccc <cuiwei_cv@163.com>
2020-04-13 11:49:48 +0800

    wakey wakey GitHub Actions

--- st7735/st7735.c
+++ st7735/st7735.c
@@ -31,6 +31,7 @@
 #include "py/mphal.h"
 #include "extmod/machine_spi.h"
 #include "st7735.h"
+#include <math.h>
 
 #define _swap_int16_t(a, b) { int16_t t = a; a = b; b = t; }
 #define _swap_bytes(val) ( (((val)>>8)&0x00FF)|(((val)<<8)&0xFF00) )
