18ee74cdd37a373e122c2aa7f2d16deeea360647
cccc <cuiwei_cv@163.com>
2020-04-15 20:02:27 +0800

    complete the bmp display.

--- st7735/test.py
+++ st7735/test.py
@@ -255,3 +255,40 @@ def test_framebuf():
     page0.chinese('关爱单身狗成长协会中', 0, 0)
     page0.show()
     
+def load_bmp_file(file, x, y):
+    global display
+    with open(file, 'rb') as f:
+        if f.read(2) == b'BM':  #header
+            dummy = f.read(8) #file size(4), creator bytes(4)
+            offset = int.from_bytes(f.read(4), 'little')
+            hdrsize = int.from_bytes(f.read(4), 'little')
+            width = int.from_bytes(f.read(4), 'little')
+            height = int.from_bytes(f.read(4), 'little')
+            if int.from_bytes(f.read(2), 'little') == 1: #planes must be 1
+                depth = int.from_bytes(f.read(2), 'little')
+                if depth == 24 and int.from_bytes(f.read(4), 'little') == 0:#compress method == uncompressed
+                    print("Image size:", width, "x", height)
+                    rowsize = (width * 3 + 3) & ~3
+                    if height < 0:
+                        height = -height
+                        flip = False
+                    else:
+                        flip = True
+                    w, h = width, height
+                    display.set_window(x, x + w - 1, y, y + h - 1)
+                    for row in range(h):
+                        if flip:
+                            pos = offset + (height - 1 - row) * rowsize
+                        else:
+                            pos = offset + row * rowsize
+                        if f.tell() != pos:
+                            dummy = f.seek(pos)
+                        for col in range(w):
+                            bgr = f.read(3)
+                            display.push_rgb_color(bgr[2], bgr[1], bgr[0])
+                else:
+                    print('not 24bit bmp.')
+                            
+def test_load_bmp_file():
+    load_bmp_file('/sd/qq_logo_24bit.bmp', 0, 0)
+    
